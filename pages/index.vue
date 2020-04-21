d5ca23fedf6c8a2d2a05898f5e9f5d909ada55bc
Piou-piou <pilloud.anthony@gmail.com>
2020-04-21 16:57:56 +0200

    [func] add method to list parcel with cows

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,26 @@
 <template>
-  <div class="container">
-    log success
-  </div>
+  <main class="ribs-container-fluid">
+    <div class="row" v-for="cowInParcel in cowsInParcel" :key="cowInParcel.id">
+      <div class="cxs-12">
+        <div class="block">
+          <header>
+            <h5>Parcelle {{cowInParcel.parcel.name}}</h5>
+          </header>
+          <div class="content">
+            <div class="row">
+              <ul class="cxs-12">
+                <li>Nombre de vaches : {{cowInParcel.cow_number}}</li>
+                <li>Entrées dans la parcelle le : {{cowInParcel.formattedStartDate}}</li>
+              </ul>
+            </div>
+          </div>
+          <footer>
+            <button class="ribs-button">Sortir les vaches</button>
+          </footer>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
 
 <script>
@@ -10,15 +29,24 @@
   export default {
     components: {
     },
+    data() {
+      return {
+        cowsInParcel: null
+      }
+    },
     mixins: [Utils],
     created() {
       this.testAndUpdateToken();
       //this.testUpdateAppVersion();
 
       if (process.client) {
-        this.getApi().post('parcels/list').then(data => {
-          console.log(data);
-        })
+        this.getApi().post('cows/list', {
+          infos: this.getJwtValues(),
+          token: this.getToken()
+        }).then(data => {
+          this.updateTokenIfExist(data.token);
+          this.cowsInParcel = data.cows_in_parcel;
+        });
       }
     }
   }
