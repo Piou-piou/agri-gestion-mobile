0d08c45327d0ed6b94fcf725c0614e2a4b3ab9db
Piou-piou <pilloud.anthony@gmail.com>
2020-04-21 16:45:24 +0200

    [func] add cows to a parcel

--- pages/add-cows.vue
+++ pages/add-cows.vue
@@ -7,19 +7,27 @@
                         <div class="row">
                             <div class="form-group cxs-12">
                                 <label>Nombre de vaches</label>
-                                <input type="number" name="cows_number" class="form-control" v-model="cows_number">
+                                <input type="number" name="cows_number" class="form-control" v-model="cowsNumber">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="form-group cxs-12">
+                                <label>Parcelle</label>
+                                <select v-model="parcel" class="form-control">
+                                    <option v-for="parcel in parcels" :key="parcel.id" v-bind:value="parcel.id">{{parcel.name}}</option>
+                                </select>
                             </div>
                         </div>
                         <div class="row">
                             <div class="form-group cxs-12">
                                 <label>Date d'entrée dans la parcelle</label>
-                                <datetime v-model="start_date" input-class="form-control"></datetime>
+                                <datetime v-model="startDate" format="dd/MM/yyyy" input-class="form-control"></datetime>
                             </div>
                         </div>
                         <div class="row">
                             <div class="form-group cxs-12">
                                 <label>Date de sortie dans la parcelle</label>
-                                <datetime v-model="end_date" input-class="form-control"></datetime>
+                                <datetime v-model="endDate" input-class="form-control"></datetime>
                             </div>
                         </div>
                         <div class="row">
@@ -31,7 +39,6 @@
                 </div>
             </div>
         </div>
-
     </main>
 </template>
 
@@ -42,11 +49,62 @@
     components: {
     },
     mixins: [Utils],
+    data() {
+      return {
+        parcel: null,
+        parcels: null,
+        cowsNumber: null,
+        startDate: null,
+        endDate: null,
+      }
+    },
+    methods: {
+      submit() {
+        if (!this.cowsNumber || !this.startDate) {
+          this.getFlash().append('Vous devez spécifiez un date de début et le nombre de vaches', 'error');
+          return false;
+        }
+
+        return this.getApi().post('cows/add', {
+          infos: this.getJwtValues({
+            'cows_number': this.cowsNumber,
+            'start_date': this.startDate,
+            'end_date': this.endDate,
+            'parcel_id': 1
+          }),
+          token: this.getToken()
+        })
+        .then((data) => {
+          if (data.success === true) {
+            this.setToken(data.token);
+            this.getFlash().append(data.success_message, 'success');
+            this.$router.push('/');
+            return;
+          } else {
+            this.getFlash().append(data.error_message, 'error');
+            return;
+          }
+        })
+        .catch(() => {
+          this.getFlash().append('Une erreur est survenue. Merci de réessayer dans un instant', 'error');
+          return;
+        });
+      }
+    },
     created() {
       this.testAndUpdateToken();
-      //this.testUpdateAppVersion();
 
       if (process.client) {
+        this.getApi().post('parcels/list', {
+          infos: this.getJwtValues(),
+          token: this.getToken()
+        }).then(data => {
+          this.updateTokenIfExist(data.token);
+          this.parcels = data.parcels;
+
+          const now = new Date();
+          this.startDate = now.toISOString();
+        });
       }
     }
   }
