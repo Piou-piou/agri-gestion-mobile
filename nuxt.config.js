0f10c01581f54ca5209f3e2cfd081b7b56fb8c29
Piou-piou <pilloud.anthony@gmail.com>
2020-04-21 12:10:21 +0200

    [func] add icon

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,7 @@ export default {
     title: 'Agri gestion',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1' },
       { hid: 'description', name: 'description', content: 'PWA for agri gestion software' }
     ],
     link: [
