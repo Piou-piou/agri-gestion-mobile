88f1d2c76ea03cd0dc6d5905d237be16956c66a1
Piou-piou <pilloud.anthony@gmail.com>
2020-04-22 15:10:18 +0200

    [func] add link to index on title and add new link in footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <header class="main ribs-container-fluid">
-      <h1>Agri Gestion</h1>
+      <h1><nuxt-link to="/">Agri Gestion</nuxt-link></h1>
     </header>
 
     <nuxt />
@@ -9,6 +9,7 @@
     <footer class="main ribs-container-fluid">
       <nav>
         <ul class="row middle-xs">
+          <li class="cxs"><nuxt-link class="row center-xs" to="/"><i class="fas fa-home fa-2x"></i></nuxt-link></li>
           <li class="cxs"><nuxt-link class="row center-xs" to="/add-cows"><i class="fas fa-plus fa-2x"></i></nuxt-link></li>
           <li class="cxs"><nuxt-link class="row center-xs" to="/parcel-list"><i class="fas fa-map-marked fa-2x"></i></nuxt-link></li>
         </ul>
